e118f6435accb19e7feca3f165fbec78bfeeff01
Apocalixs-Nick <nicolapiccirillo04@gmail.com>
2022-10-06 15:19:09 +0200

    Adding a textbutton to the town hall site
    Adding a textbutton for the town hall site on the Home screen

--- app/src/main/java/com/example/mycity/ui/test/CityHome.kt
+++ app/src/main/java/com/example/mycity/ui/test/CityHome.kt
@@ -1,22 +1,26 @@
 package com.example.mycity.ui.test
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import com.example.mycity.CityScreen
 import com.example.mycity.R
 
@@ -54,6 +58,12 @@ fun CityButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val url by remember {
+        mutableStateOf("https://www.comune.caserta.it/")
+    }
+    val site = LocalContext.current
+    val intent = Intent(Intent.ACTION_VIEW)
+    intent.data = Uri.parse(url)
     Button(
         onClick = onClick,
         modifier = modifier
@@ -62,6 +72,16 @@ fun CityButton(
         //.background(MaterialTheme.colorScheme.background)
     ) {
         Text("Start", style = MaterialTheme.typography.titleMedium)
+    }
+    Spacer(modifier = Modifier.height(45.dp))
+    TextButton(
+        onClick = { startActivity(site, intent, null) },
+        modifier = modifier
+            .widthIn(min = 250.dp)
+
+        //.background(MaterialTheme.colorScheme.background)
+    ) {
+        Text("Town Hall site", style = MaterialTheme.typography.titleMedium)
     }
 }
 
